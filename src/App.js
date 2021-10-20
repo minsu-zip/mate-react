c119aa5361fcb9948811d9b7e1dd6c632e53ad49
minsu-zip <alstn4814@gmail.com>
2021-10-20 15:30:28 +0900

    setting : antd & babel loader 에러 처리

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import styled from '@emotion/styled'
-import './App.css'
-
+import { Button } from 'antd'
+import 'antd/dist/antd.css'
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Button type="primary">Primary Button</Button>
+    </div>
+  )
 }
 
 export default App
