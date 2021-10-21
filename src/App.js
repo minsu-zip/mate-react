9ca41835d344e9cbb47500a09e9bc1cf7e4d449a
minsu-zip <alstn4814@gmail.com>
2021-10-21 16:44:09 +0900

    feat : 기본라우터 설정 및 예시 파일

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import Button from './components/Button'
 import './App.css'
-function App() {
+import { Route, Switch } from 'react-router'
+import LoginPage from '@pages/LoginPage'
+import HomePage from '@pages/HomePage'
+import PostsPage from '@pages/PostsPage'
+import MyPage from '@pages/MyPage'
+import DefaultTemplate from '@components/template/DefaultTemplate'
+import Footer from '@components/Footer'
+const App = () => {
   return (
     <div className="App">
-      <Button></Button>
+      <DefaultTemplate></DefaultTemplate>
+      <Switch>
+        <Route path="/" exact>
+          <LoginPage></LoginPage>
+        </Route>
+        <Route path="/posts" exact>
+          <PostsPage></PostsPage>
+        </Route>
+        <Route path="/home" exact>
+          <HomePage></HomePage>
+        </Route>
+        <Route path="/mypage" exact>
+          <MyPage></MyPage>
+        </Route>
+      </Switch>
+      {/* <Footer></Footer> */}
     </div>
   )
 }
---
+++ src/pages/PostsPage.js
@@ -0,0 +1,10 @@
+import Button from '@components/Button'
+const PostsPage = () => {
+  return (
+    <div>
+      <Button></Button>
+    </div>
+  )
+}
+
+export default PostsPage
