635773b2adbbc23b9e4fc3f57eaf7b7e0c8963f8
khw970421 <khw970421@kakao.com>
2021-10-22 22:15:12 +0900

    chore : 필요없는 코드 수정

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,17 +1,15 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { Form, Input, Button, Checkbox, Alert } from 'antd'
+import React, { useState } from 'react'
+import { Form, Input, Button, Checkbox } from 'antd'
 import { UserOutlined, LockOutlined } from '@ant-design/icons'
 import './index.css'
-import PostSignIn from '../Api/PostSignIn'
+import PostSignIn from '@api/PostSignIn'
+import AntAlert from '@AntDesign/AntAlert'
+import AntButton from '@AntDesign/AntButton'
 
 const Login = () => {
   const [form] = Form.useForm()
-  const [, forceUpdate] = useState({}) // To disable submit button at the beginning.
-  const [isSignInProblem, setIsSignInProblem] = useState(false)
 
-  useEffect(() => {
-    forceUpdate({})
-  }, [])
+  const [isSignInProblem, setIsSignInProblem] = useState(false)
 
   const onFinish = (values) => {
     PostSignIn({ id: values.username, pw: values.password }).then((res) => {
@@ -21,71 +19,76 @@ const Login = () => {
   }
 
   return (
-    <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Username!',
-          },
-        ]}
+    <>
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
       >
-        <Input
-          prefix={<UserOutlined className="site-form-item-icon" />}
-          placeholder="Username"
-        />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Password!',
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Item>
-      {isSignInProblem ? (
-        <Alert
-          message="Error"
-          description="이메일 혹은 비밀번호를 잘못 입력했습니다"
-          type="error"
-          showIcon
-        />
-      ) : (
-        ''
-      )}
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Username!',
+            },
+          ]}
+        >
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your Password!',
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        {isSignInProblem ? (
+          <AntAlert
+            message={'Error'}
+            description={'계정 정보가 틀립니다!'}
+            type={'error'}
+          ></AntAlert>
+        ) : (
+          ''
+        )}
 
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
         </Form.Item>
 
-        <a className="login-form-forgot" href="">
-          Forgot password
-        </a>
-      </Form.Item>
-
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
-        </Button>
-        Or <a href="/register">signUp now!</a>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <AntButton
+            text="Log in"
+            type="primary"
+            size="large"
+            htmlType="submit"
+            className="login-form-button"
+          />
+          Or <a href="/register">signUp now!</a>
+        </Form.Item>
+      </Form>
+    </>
   )
 }
 
