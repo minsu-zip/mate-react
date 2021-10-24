f46d40e2d770ce40bd547363b9a93d5ae595519b
minsu-zip <alstn4814@gmail.com>
2021-10-24 21:30:23 +0900

    setting : storybook에서 antDesign 사용 할 수 있게 환경설정

---
+++ src/components/Loading/index.js
@@ -0,0 +1,10 @@
+import { Spin } from 'antd'
+const Loading = () => {
+  return (
+    <>
+      <Spin />
+    </>
+  )
+}
+
+export default Loading
---
+++ src/stories/Loading.stories.jsx
@@ -0,0 +1,10 @@
+import Loading from '@components/Loading'
+
+export default {
+  title: 'Component/Loading',
+  component: Loading,
+}
+
+export const Default = (args) => {
+  return <Loading {...args} />
+}
