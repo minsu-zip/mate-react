273b38289ad08b304138d596843ab2f8ee9bd53f
minsu-zip <alstn4814@gmail.com>
2021-10-24 19:44:19 +0900

    feat : 404 page 개발

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,5 +1,21 @@
+import { Result, Button } from 'antd'
 const NotFoundPage = () => {
-  return <div>404 Not Found</div>
+  return (
+    <div>
+      {' '}
+      <Result
+        status="404"
+        title="404"
+        subTitle="Sorry, the page you visited does not exist."
+        extra={
+          <Button type="primary">
+            <a href="/">Back Home</a>
+          </Button>
+        }
+      />
+      ,
+    </div>
+  )
 }
 
 export default NotFoundPage
