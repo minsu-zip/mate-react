1bc238fb8329ced8de0637a5a2eeea31484cb1e7
minsu-zip <alstn4814@gmail.com>
2021-10-26 00:55:22 +0900

    feat : 댓글 기능 로직 추가

--- src/components/Comment/CommentList.jsx
+++ src/components/Comment/CommentList.jsx
@@ -1,61 +1,32 @@
-import { Tooltip, List } from 'antd'
-import moment from 'moment'
+import { List } from 'antd'
 import CommentItem from './CommentItem'
-import React from 'react'
-
-const data = [
-  {
-    actions: [<span key="comment-list-reply-to-0">Reply to</span>],
-    author: 'Han Solo',
-    avatar: 'https://joeschmoe.io/api/v1/random',
-    content: (
-      <p>
-        We supply a series of design principles, practical patterns and high
-        quality design resources (Sketch and Axure), to help people create their
-        product prototypes beautifully and efficiently.
-      </p>
-    ),
-    datetime: (
-      <Tooltip
-        title={moment().subtract(1, 'days').format('YYYY-MM-DD HH:mm:ss')}
-      >
-        <span>{moment().subtract(1, 'days').fromNow()}</span>
-      </Tooltip>
-    ),
-  },
-  {
-    actions: [<span key="comment-list-reply-to-0">Reply to</span>],
-    author: 'Han Solo',
-    avatar: 'https://joeschmoe.io/api/v1/random',
-    content: (
-      <p>
-        We supply a series of design principles, practical patterns and high
-        quality design resources (Sketch and Axure), to help people create their
-        product prototypes beautifully and efficiently.
-      </p>
-    ),
-    datetime: (
-      <Tooltip
-        title={moment().subtract(2, 'days').format('YYYY-MM-DD HH:mm:ss')}
-      >
-        <span>{moment().subtract(2, 'days').fromNow()}</span>
-      </Tooltip>
-    ),
-  },
-]
+import React, { useEffect, useState } from 'react'
 
 const CommentList = ({ comment }) => {
-  console.log(comment)
+  const [commentList, setCommentList] = useState('')
+
+  useEffect(() => {
+    const commentData = comment.map(({ comment, author, createdAt }) => {
+      return {
+        author: author.email,
+        avatar: 'https://joeschmoe.io/api/v1/random',
+        content: <p>{comment}</p>,
+        datetime: createdAt,
+      }
+    })
+
+    setCommentList(commentData)
+  }, [])
+
   return (
     <>
       <List
+        style={{ marginBottom: '20px' }}
         className="comment-list"
-        header={`${data.length} replies`}
         itemLayout="horizontal"
-        dataSource={data}
+        dataSource={commentList}
         renderItem={(item) => <CommentItem item={item}></CommentItem>}
       />
-      ,
     </>
   )
 }
