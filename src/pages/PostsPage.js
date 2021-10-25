ff6db0f3cec46a1626a95759dc86ff02efec889f
minsu-zip <alstn4814@gmail.com>
2021-10-26 03:25:09 +0900

    design : 컴포넌트 디자인 수정

--- src/pages/PostsPage.js
+++ src/pages/PostsPage.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import PostList from '@components/Post/PostList'
-import { Button } from 'antd'
+import styled from '@emotion/styled'
+
+const Container = styled.div`
+  /* width: 80%; */
+  margin-left: 50px;
+  margin-right: 50px;
+`
 
 const PostsPage = () => {
   return (
     <>
-      <PostList></PostList>
+      <Container>
+        <PostList></PostList>
+      </Container>
     </>
   )
 }
