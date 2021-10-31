5a0d78157c2b7757c9680ffa69a7994255274a39
minsu-zip <alstn4814@gmail.com>
2021-10-31 14:28:34 +0900

    feat : user 정보 세션스토리지 추가

--- src/SessionStorage/index.js
+++ src/SessionStorage/index.js
@@ -7,6 +7,9 @@ export const getItem = (target) => {
 
 export const setItem = (target, response) => {
   sessionStorage.setItem(target, JSON.stringify(response.data.token))
+  sessionStorage.setItem('userEmail', JSON.stringify(response.data.user.email))
+  sessionStorage.setItem('userId', JSON.stringify(response.data.user._id))
+  sessionStorage.setItem('userImage', JSON.stringify(response.data.user.image))
 }
 
 export const removeItem = (target) => {
