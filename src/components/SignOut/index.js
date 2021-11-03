a81715032638ee86da4146b5c9eba8563f2810df
khw970421 <khw970421@kakao.com>
2021-11-03 17:01:17 +0900

    chore : posts 페이지 Header CSS 수정

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,17 +1,17 @@
 import { Link } from 'react-router-dom'
 import { getRequest } from '@api/index.js'
-
-const SignOut = ({ style }) => {
+import { Button } from 'antd'
+const SignOut = () => {
   return (
     <Link to="/">
-      <button
+      <Button
+        style={{ margin: '10px 5px 10px 5px' }}
         onClick={() => {
           getRequest('logout')
         }}
-        style={style}
       >
         Logout
-      </button>
+      </Button>
     </Link>
   )
 }
--- src/pages/PostsPage.js
+++ src/pages/PostsPage.js
@@ -1,11 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import PostList from '@components/Post/PostList'
 import styled from '@emotion/styled'
-import Header from '@components/Header'
+// import Header from '@components/Header'
 import HeaderChannelButtons from '@components/HeaderChannelButtons'
 import GetChannelInfo from '@api/GetChannelInfo'
+import { Layout, Menu, Breadcrumb, message } from 'antd'
+import { Input, Space, Avatar, Button } from 'antd'
+import { AudioOutlined } from '@ant-design/icons'
+import { getItem } from '@SessionStorage'
 import axios from 'axios'
-import { message } from 'antd'
+import { useHistory } from 'react-router-dom'
+import SignOut from '@components/SignOut'
+import MyPage from '@components/MyPageButton'
+
+const { Header, Content, Footer } = Layout
+const { Search } = Input
+const API_END_POINT = 'http://13.209.30.200'
+
+const onSearch = (value) => console.log(value)
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: 'white';
+`
 
 const Container = styled.div`
   width: 100%;
@@ -22,17 +41,14 @@ const ChannelButton = styled.div`
   justify-content: center;
   border: 1px solid lightgray;
   white-space: nowrap;
-  overflow-x: scroll;
   flex-wrap: nowrap;
 `
-const searchValue = 100
-const API_END_POINT = 'http://13.209.30.200'
 
 const PostsPage = () => {
+  const history = useHistory()
   const [channelList, setChannelList] = useState([])
   const [selectChannel, setSelectChannel] = useState('616a200d22996f0bc94f6db5')
   let searchChannelId
-
   useEffect(() => {
     GetChannelInfo().then((res) => {
       setChannelList(res)
@@ -42,6 +58,8 @@ const PostsPage = () => {
   const channelClick = (e) => {
     setSelectChannel(e.target.className)
   }
+
+  const userImg = getItem('userImage')
 
   const onClickSearchBtn = async (value) => {
     const channelData = await axios
@@ -62,10 +80,27 @@ const PostsPage = () => {
 
   return (
     <>
-      <Header
-        searchValue={searchValue}
-        onClickSearchBtn={onClickSearchBtn}
-      ></Header>
+      <>
+        <div
+          style={{ backgroundColor: '#E2E2E2', padding: '10px 0px 10px 0px' }}
+        >
+          <HeaderContainer>
+            <img src="https://ifh.cc/g/XTsvNi.png" style={{ height: '70px' }} />
+
+            <Search
+              placeholder="input search text"
+              onSearch={onClickSearchBtn}
+              style={{ width: 400, backgroundColor: 'white' }}
+            />
+            <div>
+              <Avatar size={64} src={userImg} />
+
+              <MyPage />
+              <SignOut />
+            </div>
+          </HeaderContainer>
+        </div>
+      </>
 
       <ChannelButton>
         <HeaderChannelButtons
