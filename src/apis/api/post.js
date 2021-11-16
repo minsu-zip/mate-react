d79fe97f93bb34c2faad40fe243c477b3cfde1cf
minsu-zip <alstn4814@gmail.com>
2021-11-17 01:10:49 +0900

    refactor : 포스트 생성, 삭제 api 로직 분리

--- src/apis/api/post.js
+++ src/apis/api/post.js
@@ -11,6 +11,22 @@ export const getPost = async (selectChannel) => {
   }
 }
 
+export const postCreate = async (formData) => {
+  try {
+    await authInstance.post('posts/create', formData)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const postUpdate = async (formData) => {
+  try {
+    await authInstance.put('posts/update', formData)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 export const deletePost = async (id) => {
   try {
     await authInstance.delete('posts/delete', { data: id })
